d45194734018e8b5eee2da65e0be010d09df2491
tydtbd <zldzkd39@gmail.com>
2016-12-27 17:20:59 +0900

    12월27일

---
+++ 개발/JAVA/Test.java
@@ -0,0 +1,13 @@
+//파일명 : Test.java
+
+class Test
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World!");
+  }
+}
+//
+// 번역방법:javac Test.javac
+// 실행방법:java Test
+//
